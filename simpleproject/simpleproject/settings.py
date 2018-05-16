a7e3afb60dc7fc9ae1092da4a73c13b2ad65d355
Andrew Shvedov <andreyw441@gmai.com>
2018-05-16 12:07:54 +0300

    Cleaning

--- simpleproject/simpleproject/settings.py
+++ simpleproject/simpleproject/settings.py
@@ -28,11 +28,11 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
 
 # Application definition
 
